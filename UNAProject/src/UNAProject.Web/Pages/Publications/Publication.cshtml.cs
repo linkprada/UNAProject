c3a05caec139705628b50709989d9e292fcb4703
linkprada <haouaouchi.said@gmail.com>
2022-05-01 01:37:43 +0100

    Display Publication Attchments of type Image

--- UNAProject/src/UNAProject.Web/Pages/Publications/Publication.cshtml.cs
+++ UNAProject/src/UNAProject.Web/Pages/Publications/Publication.cshtml.cs
@@ -1,8 +1,14 @@
+// <copyright file="Publication.cshtml.cs" company="linkprada">
+// Copyright (c) linkprada. All rights reserved.
+// </copyright>
+
+using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Threading.Tasks;
 using UNAProject.Core.Entities.PublicationAggregate;
+using UNAProject.Core.Entities.PublicationAggregate.Specifications;
 using UNAProject.SharedKernel.Interfaces;
 using UNAProject.Web.Models;
 
@@ -11,7 +17,6 @@ namespace UNAProject.Web.Pages.Publications
     public class PublicationModel : PageModel
     {
         private readonly IReadRepository<Publication> _publicationRepository;
-
         private readonly IMapper _mapper;
 
         public PublicationModel(IReadRepository<Publication> publicationRepository, IMapper mapper)
@@ -22,9 +27,12 @@ namespace UNAProject.Web.Pages.Publications
 
         public PublicationDTO Publication { get; set; }
 
+        public PublicationType PublicationType { get; set; }
+
         public async Task<IActionResult> OnGetAsync(int id)
         {
-            var publicationFound = await _publicationRepository.GetByIdAsync(id);
+            var publicationSpec = new PublicationWithAttachmentsSpecification(id);
+            var publicationFound = await _publicationRepository.GetBySpecAsync(publicationSpec);
             if (publicationFound is null)
             {
                 return LocalRedirect("/");
