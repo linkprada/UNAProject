28fd733d875d031a968002b0b824a4c7b3e95c2a
linkprada <haouaouchi.said@gmail.com>
2022-05-14 21:54:45 +0100

    Add Pagination in Publication Page

--- UNAProject/src/UNAProject.Web/Pages/Publications/Index.cshtml.cs
+++ UNAProject/src/UNAProject.Web/Pages/Publications/Index.cshtml.cs
@@ -7,8 +7,11 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using UNAProject.Core.Entities.PublicationAggregate;
+using UNAProject.Core.Entities.PublicationAggregate.Specifications;
 using UNAProject.SharedKernel.Interfaces;
+using UNAProject.Web.Interfaces;
 using UNAProject.Web.Models;
+using UNAProject.Web.ViewModels;
 
 namespace UNAProject.Web.Pages.Publications
 {
@@ -16,20 +19,29 @@ namespace UNAProject.Web.Pages.Publications
     {
         private readonly IReadRepository<Publication> _publicationRepository;
         private readonly IMapper _mapper;
+        private readonly IPaginationService _paginationService;
 
-        public IndexModel(IReadRepository<Publication> publicationRepository, IMapper mapper)
+        public IndexModel(IReadRepository<Publication> publicationRepository, IMapper mapper, IPaginationService paginationService)
         {
             _publicationRepository = publicationRepository;
             _mapper = mapper;
+            _paginationService = paginationService;
         }
+
+        public PaginationInfosViewModel PaginationViewModel { get; set; }
 
         public List<PublicationDTO> Publications { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int pageIndex = 1)
         {
-            var allPublications = await _publicationRepository.ListAsync();
+            var publicationsPaginatedSpec = new PublicationPaginatedSpecification((pageIndex - 1) * Constants.ITEMS_PER_PAGE, Constants.ITEMS_PER_PAGE);
 
-            Publications = _mapper.Map<List<PublicationDTO>>(allPublications);
+            var retrievedPublications = await _publicationRepository.ListAsync(publicationsPaginatedSpec);
+
+            Publications = _mapper.Map<List<PublicationDTO>>(retrievedPublications);
+
+            var totalPublicationsCount = await _publicationRepository.CountAsync();
+            PaginationViewModel = _paginationService.CreatePaginationInfos(pageIndex, Publications.Count, totalPublicationsCount);
         }
     }
 }
